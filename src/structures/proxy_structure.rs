271d7ad0814c12587faab7490ca327568ba42dbd
Linus Walker <rissk_it@proton.me>
2025-02-12 21:07:32 +0630

    refactor: restructure proxy

---
+++ src/structures/proxy_structure.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct ProxyConfig {
+    pub listener: String,
+    pub tls_certificate: Option<String>,
+    pub tls_certificate_key: Option<String>,
+    pub servers: HashMap<String, ProxyHostConfig>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ProxyHostConfig {
+    pub proxy_addr: String,
+    pub proxy_tls: bool,
+    pub proxy_headers: Option<Vec<(String, String)>>,
+    pub proxy_uds: Option<bool>,
+    pub routes: Option<HashMap<String, ProxyPathBaseHostConfig>>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ProxyPathBaseHostConfig {
+    pub proxy_addr: Option<String>,
+    pub proxy_tls: Option<bool>,
+    pub proxy_headers: Option<Vec<(String, String)>>,
+    pub proxy_uds: Option<bool>,
+}
