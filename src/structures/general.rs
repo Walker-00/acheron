6f755b2e6085b7350a6faad9d02f657390c08299
Linus Walker <rissk_it@proton.me>
2025-02-12 21:09:35 +0630

    refactor: restructure general

---
+++ src/structures/general.rs
@@ -0,0 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+use super::{load_balancer_structure::LoadBalancerConfig, proxy_structure::ProxyConfig};
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub prometheus_addr: Option<String>,
+    pub proxy: Option<Vec<ProxyConfig>>,
+    pub load_balancer: Option<Vec<LoadBalancerConfig>>,
+}
